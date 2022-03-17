8f3d8b20bcda3b7414112e4c5fb68996a3bf6612
dennis <denhouston1@icloud.com>
2022-03-17 18:38:48 +0000

    updates

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -4,7 +4,10 @@ describe Bookmark do
   describe '.all' do
     it 'returns all bookmarks' do
       bookmarks = Bookmark.all
-      expect(bookmarks).to include('google.com')
+
+      expect(bookmarks).to include('http://www.makersacademy.com/')
+      expect(bookmarks).to include('http://www.google.com')
+      expect(bookmarks).to include('http://www.facebook.com')
     end
   end
 end
--- spec/features/view_bookmarks_spec.rb
+++ spec/features/view_bookmarks_spec.rb
@@ -1,6 +1,8 @@
 feature "view bookmarks" do
   scenario "displays a list of bookmarks" do
     visit('/bookmarks')
-    expect(page).to have_content("google.com")
+    expect(page).to have_content('http://www.makersacademy.com/')
+    expect(page).to have_content('http://www.google.com')
+    expect(page).to have_content('http://www.facebook.com')
   end
 end
